d401b9fd21a3eaa5d1b25967a49b9886272075a6
AlanHN <546004464@qq.com>
2022-06-07 22:45:51 +0800

    fix: run.sh

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # WeGroupon
 
 WeGroupon is a application for people to groupon things
+
+#How to use
+
+```shell
+pip3 install -r requeriments.txt
+
+./run.sh 
+```
--- run.sh
+++ run.sh
@@ -4,9 +4,27 @@ set -euxo pipefail
 
 protoc -I=./proto/ --python_out=./ ./proto/*.proto
 
+if [ $# -ne 1 ] && [ $# -ne 2 ]
+then
+   echo "USAGE: ./run.sh gui(cli) appID(optional)"
+   exit 1;
+fi
+
 if [ $1 == 'gui' ]
 then
+      if [ $# == 2 ]
+      then
+         ./gui.py $2
+      else
+         ./gui.py
+      fi
    ./gui.py $2
-else
-   ./main.py $2
+elif [ $1 == 'cli' ]
+then
+      if [ $# == 2 ]
+      then
+         ./main.py $2
+      else
+         ./main.py
+      fi
 fi
